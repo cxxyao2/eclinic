0027ec3d873ed847e95b9504528004602fed639d
jennifer <cxxyao2@gmail.com>
2025-03-07 16:49:15 -0500

    feat: update API base URL to production endpoint

--- src/env.js
+++ src/env.js
@@ -1,5 +1,6 @@
 (function (window) {
   window.__env = window.__env || {};
   // API url
-  window.__env.BaseURL = "http://localhost:5215";
+  window.__env.BaseURL =
+    "https://eclinic-bakend-bxeea6h2gpaug0dz.canadacentral-01.azurewebsites.net";
 })(this);
